5d0d85c8460d19b3c4e353b1b38ffa26c330ea12
wendellmoraisz <wendellmorais3319@gmail.com>
2023-09-08 20:24:56 -0300

    feat: Email Register Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { ThemeProvider } from "@emotion/react"
+import RegisterEmail from "./pages/register-email"
 import { muiTheme } from "./muiThemeConfig"
 
 function App() {
 
   return (
     <ThemeProvider theme={muiTheme}>
-      ola
+      <RegisterEmail />
     </ ThemeProvider>
   )
 }
---
+++ src/pages/register-email/index.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import IFPALogo from "../../assets/ifpa_logo.svg";
+
+const RegisterEmail = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email")
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 15,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center"
+        }}
+      >
+        <Box sx={{
+          width: 200,
+        }}>
+          <img src={IFPALogo} />
+        </Box>
+        <Typography component="h1" variant="h6" fontWeight="bold" align="center">
+          Inscreva-se para receber as notícias do IFPA Campus Altamira direto na sua caixa de e-mails
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="email"
+            label="Seu principal e-mail"
+            type="email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            required
+            InputProps={{
+              endAdornment: (
+                <EmailOutlinedIcon
+                  color="primary"
+                  sx={{ mr: 1 }}
+                />
+              )
+            }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Quero me inscrever
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
+
+export default RegisterEmail;
